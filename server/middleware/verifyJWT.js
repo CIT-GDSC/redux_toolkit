09dce358df94d46640b8fc17a34b90f14823543f
Domains18 <gibsonsgibson88@gmail.com>
2023-04-23 18:32:33 +0300

    verify JWT middleware
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

---
+++ server/middleware/verifyJWT.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (!authHeader?.startsWith('Bearer')) return res.sendStatus(401);
+    const token = authHeader.split(' ')[1];
+    console.log(token);
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.sendStatus(403); //invalid token 
+        req.user = decoded.userInfo.username;
+        req.roles = decoded.userInfo.roles;
+        next();
+    });
+}
+
+
+module.exports = verifyJWT;
