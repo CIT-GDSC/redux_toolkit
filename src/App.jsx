e33689a7e8f38209254406ee25ea59e6dd4927ac
Domains18 <gibsonsgibson88@gmail.com>
2023-04-23 16:48:15 +0300

    routing at app
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react"
+
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Public from "./components/Public";
+import Login from './features/auth/Login';
+import Welcome from "./features/auth/Welcome";
+import RequireAuth from "./features/auth/RequireAuth";
+
+
+
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={Layout}>
+        {/* Public routes (nested) */}
+        <Route index element={<Public />} />
+        <Route path="/login" element={<Login />} />
+        {/* Protected Routes */}
+        <Route element={<RequireAuth />}>
+          <Route path="welcome" element={<Welcome/>}/>
+        </Route>
+      </Route>
+    </Routes>
   )
 }
 
