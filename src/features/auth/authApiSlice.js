238bf9efb12431f6d67e99f4f502de1e605a0578
Domains18 <gibsonsgibson88@gmail.com>
2023-04-23 11:57:23 +0300

    authentication api
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/features/auth/authApiSlice.js
+++ src/features/auth/authApiSlice.js
@@ -1,1 +1,14 @@
-i
+import { apiSlice } from "../../app/api/apiSlice";
+
+
+export const authApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        login: builder.mutation({
+            query: credentials => ({
+                url: '/auth',
+                method: 'POST',
+                body: {...credentials}
+            })
+        }),
+    })
+});
