55905581e27c2a927be42282052729e9c434222e
Domains18 <gibsonsgibson88@gmail.com>
2023-04-23 12:29:11 +0300

    routing
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
