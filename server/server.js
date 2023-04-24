ce81a194d1daa5c4cdeae686edaf5e1b088a6896
Domains18 <gibsonsgibson88@gmail.com>
2023-04-24 15:36:05 +0300

    server setup
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- server/server.js
+++ server/server.js
@@ -8,10 +8,23 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
-
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const credentials = require('./middleware/credentials');
 
 // Connect to MongoDB
 connectDB();
+
+//custom middleware logger
+app.use(logger);
+
+//handle options credentials check 
+//fetch cookies credentials requirement
+app.use(credentials);
+
+//Cross Origin Resource Sharing
+app.use(cors(corsOptions));
 
 
 
@@ -24,6 +37,32 @@ app.use(cors(corsOptions));
 
 // built in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: true }));
+
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+//routes
+app.use('/', require('./routes/root'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/refresh'.require('/routes/refresh'));
+app.use('/logout', require('./routes/logout'));
+
+app.use(verifyJWT);
+app.use('/employees', require('./routes/api/employees'));
+app.use('/users', require('./routes/api/users'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: 'Not found' });
+    } else if (req.accepts('text')) {
+        res.type('txt').send('404 Not found');
+    }
+});
+app.use(errorHandler);
 
 mongoose.connection.once('open', () => { console.log('Connected to MongoDB') });
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
